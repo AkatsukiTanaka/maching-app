4bb58334e3969ab4e1aa3d0aac61009de5fcfa64
AkatsukiTanaka <shibaluke5526@gmail.com>
2021-03-05 11:50:31 +0900

    heroku https force

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // herokuでhttpsを矯正する
+        if (\App::environment('production')) {
+          \URL::forceScheme('https');
+        }
     }
 }
